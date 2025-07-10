3b26de285d2faa36a2bb3a6c77700617a4cd7cf4
luigipascal <luigi@rondanini.com>
2025-07-10 05:00:22 +0100

    Create App.js

---
+++ src/app/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Maira Consultancy</h1>
+      <p>This is the main entry point of your app.</p>
+    </div>
+  );
+}
+
+export default App;
